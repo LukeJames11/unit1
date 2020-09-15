4f53cd95197039677d707b8608f7dbefd9253232
LukeJames11 <828406@mystma.org>
2020-09-15 19:34:27 +0000

    all done with fav

---
+++ unit1/src/app/App.java
@@ -0,0 +1,20 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("will stinks");
+        System.out.println("boo");
+        System.out.println("lord of rings");
+        System.out.println("thor");
+        System.out.println("iron man");
+        System.out.println("bee movie");
+        System.out.println("joker");
+        System.out.println("shrek");
+        System.out.println("the lego movie");
+        System.out.println("the sonic");
+        System.out.println("barbie and her dream house");
+        System.out.println("cars");
+        System.out.println("jaws");    
+        
+    }
+}
