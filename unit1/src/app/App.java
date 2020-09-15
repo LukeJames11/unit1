f26a0d6787326ceb67ed24a8e27d4e4a3d2726bc
LukeJames11 <828406@mystma.org>
2020-09-15 20:17:25 +0000

    i almost done

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,20 +1,26 @@
 package app;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("will stinks");
-        System.out.println("boo");
-        System.out.println("lord of rings");
-        System.out.println("thor");
-        System.out.println("iron man");
-        System.out.println("bee movie");
-        System.out.println("joker");
-        System.out.println("shrek");
-        System.out.println("the lego movie");
-        System.out.println("the sonic");
-        System.out.println("barbie and her dream house");
-        System.out.println("cars");
-        System.out.println("jaws");    
-        
+    public static void main(String[] args) throws Exception { 
+        boolean livesInAlbertville = false;
+
+        System.out.println("do i live in albertville?");
+        System.out.println(livesInAlbertville);
+        byte howManyFingersIHave = 10;
+
+        System.out.println("how many finger do i have?");
+        System.out.println(howManyFingersIHave);
+        char statesInUnitedstates = 'f';
+
+        System.out.println("how many states are in the unitedstates?");
+        System.out.println(statesInUnitedstates);
+        short willsFavNum = 23475;
+
+        System.out.println("what is wills favorite number");
+        System.out.println(willsFavNum);
+        long bigNum = 32489372;
+
+        System.out.println("what is a big number");
+        System.out.println(bigNum);
     }
 }
