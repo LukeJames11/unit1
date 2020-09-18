2de18fe8de16e549dc347b27808f1e95db31c286
LukeJames11 <828406@mystma.org>
2020-09-18 02:45:10 +0000

    finished assignment

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -2,25 +2,24 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception { 
-        boolean livesInAlbertville = false;
+     String person1 = "kyle";
+     String verb1 = "eat";
+     String place1 = "dunkin donuts";
 
-        System.out.println("do i live in albertville?");
-        System.out.println(livesInAlbertville);
-        byte howManyFingersIHave = 10;
+     System.out.println(person1 + " " + "likes too" + " " + verb1 + " " + "at" + " " + place1);
 
-        System.out.println("how many finger do i have?");
-        System.out.println(howManyFingersIHave);
-        char statesInUnitedstates = 'f';
+     String person2 = "john";
+     String verb2 = "race";
+     String place2 = "town";
+     String adjective1 = "fast";
 
-        System.out.println("how many states are in the unitedstates?");
-        System.out.println(statesInUnitedstates);
-        short willsFavNum = 23475;
+     System.out.println(person2 + " " + "likes to" + " " + verb2 + " " + "all round" + " " + place2 + " " + "because he thinks hes really" + " " + adjective1);
 
-        System.out.println("what is wills favorite number");
-        System.out.println(willsFavNum);
-        long bigNum = 32489372;
+     String food = "chinese";
+     String verb3 = "jumpy";
+     String adjective2 = "yummy";
+     String dayOfWeek = "sundays";
 
-        System.out.println("what is a big number");
-        System.out.println(bigNum);
+     System.out.println("on" + " " + dayOfWeek + " " + "i get very" + " " + verb3 + " " + "becuase we sometimes get to have" + " " + food + " " + "and it is very very" + " " + adjective2);
     }
 }
