b44ae226c143dcac1feee0f2d385c955536fcf6c
LukeJames11 <828406@mystma.org>
2020-09-25 18:29:21 +0000

    5 for loops done

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,25 +1,28 @@
 package app;
 
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("please enter color");
-        String   color = input.nextLine();
-
-        System.out.println("please enter a number");
-        int number = input.nextInt();
-
-        int count = 0;
-
-        while(count < number) {
-            System.out.println(color);
-            count = count + 1;
+        for (int i = 0; i <= 10; i = i +1){
+            System.out.println("the count is " + i);
+            System.out.println("lets keep counting");
+        }
+        for (int i = 10; i <= 20; i = i +1){
+            System.out.println("the count is " + i);
+            System.out.println("lets keep counting");
+        }
+        for (int i = 1000; i <= 256000; i = i +1000){
+            System.out.println("the count is " + i);
+            System.out.println("lets keep counting");
+        }
+        for (int i = 11; i <= 12; i = i +1){
+            System.out.println("the count is " + i);
+            System.out.println("lets keep counting");
+        }
+        for (int i = 10; i <= 0; i = i - 1){
+            System.out.println("the count is " + i);
+            System.out.println("lets keep counting");
         }
     
-    }
     }
 }
