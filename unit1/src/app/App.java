aea906dd0560aebe103b4cfec2e5c948318e9d5d
LukeJames11 <828406@mystma.org>
2020-10-01 23:38:34 +0000

    part done with animal facts

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -5,56 +5,12 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Scanner input = new Scanner(System.in);
+       Scanner input = new Scanner(System.in);
 
-        System.out.println("what is 10/2");
-        String answer1 = input.nextLine();
-        if (answer1.equals("5")){
-            System.out.println("you have the right answer");
-        }
-        else{
-        System.out.println("you have the wrong answer");
-        }
+        System.out.println("welcome to luke facts pick a number  1-5");
 
-         System.out.println("what is 5 * 5");
-        String answer2 = input.nextLine();
-        if (answer2.equals("25")){
-            System.out.println("you have the right answer");
+        String answer1 = imput.nextLine();
+        if (answer1.equals("1")){
+            System.out.println("lukes fav color is green");
         }
-        else{
-        System.out.println("you have the wrong answer");
         }
-
-        System.out.println("what is 100/5");
-        String answer3 = input.nextLine();
-        if (answer3.equals("20")){
-            System.out.println("you have the right answer");
-        }
-        else{
-        System.out.println("you have the wrong answer");
-        }
-
-                 System.out.println("what is 6 * 7");
-        String answer4 = input.nextLine();
-        if (answer4.equals("42")){
-            System.out.println("you have the right answer");
-        }
-        else{
-        System.out.println("you have the wrong answer");
-        }
-
-                 System.out.println("what is 400 + 267000");
-        String answer5 = input.nextLine();
-        if (answer5.equals("267400")){
-            System.out.println("you have the right answer");
-        }
-        else{
-        System.out.println("you have the wrong answer");
-        }
-
-
-
-
-        System.out.println("all finished");
-    }
-}
