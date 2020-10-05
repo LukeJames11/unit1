2c5bc932c2c940eaa4ef2a65b8cad8a86acf531c
LukeJames11 <828406@mystma.org>
2020-10-05 20:09:27 +0000

    almost done 2nd challenge

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -5,31 +5,21 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 
-       Scanner input = new Scanner(System.in);
+                System.out.println("whats the pasword");
 
-        System.out.println("welcome to luke facts pick a number  1-5");
+                        Scanner input = new Scanner(System.in);
 
         String answer1 = input.nextLine();
 
-        if (answer1.equals("1")){
-            System.out.println("lukes fav color is green");
-        }
-        if (answer1.equals("2")){
-            System.out.println("luke has 1  dog its a red lab");
-        }
-        if (answer1.equals("3")){
-            System.out.println("luke is 16 years old");
-        }
-        if (answer1.equals("4")){
-            System.out.println("luke has 1 older sister and one younger sister");
-        }
-        if (answer1.equals("5")){
-            System.out.println("luke is in grade 10 proggraming class");
-        }
-        
-        else{
-        System.out.println("ERROR!!!!!!!!!!!!!!!! dont you ever click this number");
+        if (answer1.equals("0000")){
+            System.out.println("that is the right pasword");
         }
 
+        else{
+        System.out.println("ERROR!");
+                System.out.println("please enter password");
         }
+
+
     }
+}
