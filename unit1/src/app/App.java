f614606f64295be73706f7323f7964645b262f5b
LukeJames11 <828406@mystma.org>
2020-10-05 21:09:49 +0000

    done with 2nd challenge

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -13,13 +13,30 @@ public class App {
 
         if (answer1.equals("0000")){
             System.out.println("that is the right pasword");
+            System.out.println("i like to run cc");
         }
 
         else{
         System.out.println("ERROR!");
                 System.out.println("please enter password");
+                String answer2 = input.nextLine();
+                if (answer2.equals("0000")){
+            System.out.println("that is the right pasword");
+            System.out.println("i like to run cc");
+                }
+
+            else{
+        System.out.println("ERROR LAST CHANCE!");
+                System.out.println("please enter password");
+                String answer3 = input.nextLine();
+                if (answer3.equals("0000")){
+            System.out.println("that is the right pasword");
+            System.out.println("i like to run cc");
+                }
+                else{
+                System.out.println("ERROR! BYE!!!");
+                }
         }
-
-
     }
+}
 }
