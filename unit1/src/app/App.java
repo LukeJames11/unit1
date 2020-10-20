be3b7271834fbf26e8d1aa8425af9ccffb8f36c6
LukeJames11 <828406@mystma.org>
2020-10-20 20:14:43 +0000

    fg

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,15 +1,36 @@
 package app;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
  Scanner input = new Scanner(System.in);
+ Random rnd = new Random();
 
-        System.out.println("you have  100 $1 chips and ten rounds would you like to make a inside or outside bet ");
+        System.out.println("would you like to make a inside or outside bet");
+            String answer1 = input.nextLine();
 
-            int answer1 = input.nextInt();
+             if (answer1.equals("inside")){
+                 System.out.println("what number would you like to bet on");
+                 int answer2 = input.nextInt();
+                 int n2 = rnd.nextInt(37);
+                 System.out.println("genrated number is " + n2);
+
+                 if(n2 == answer2){
+                     System.out.println("your right!");
+                 }
+                 else{
+                     // red and blackroll
+                 }
+
+             }
+        
+
+        
+            else{
+        }
 
 
   }
