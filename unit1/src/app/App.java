4cf5911fe2db3c29f79d61fcdcc0265a11588dd1
LukeJames11 <828406@mystma.org>
2020-11-04 21:19:32 +0000

    i will finish tonight

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -6,73 +6,73 @@ import java.util.Random;
 public class App {
     public static void main(String[] args) throws Exception {
 
- Scanner input = new Scanner(System.in);
- Random rnd = new Random();
- var bank = 100;
- 
- while(bank > 0){
+        Scanner input = new Scanner(System.in);
+        Random rnd = new Random();
+        var i = 10;
 
-        System.out.println("would you like to make a inside or outside bet");
+        while (i > 0) {
+
+            System.out.println("would you like to make a inside or outside bet");
             String answer1 = input.nextLine();
 
-             if (answer1.equals("inside")){
-                 System.out.println("what number would you like to bet on");
-                 int answer2 = input.nextInt();
-                 int n1 = rnd.nextInt(37);
-                 System.out.println("genrated number is " + n1);
+            if (answer1.equals("inside")) {
+                System.out.println("what number would you like to bet on");
+                int answer2 = input.nextInt();
+                input.nextLine();
+                int n1 = rnd.nextInt(37);
+                System.out.println("genrated number is " + n1);
 
-                 if(n1 == answer2){
-                     System.out.println("your right!");
-                     System.out.println("if you want to bet again say bet if you would like to withdraw say withdraw");
-                     String answer3 = input.nextLine();
-                     if(answer3.equals("bet")){
-                         System.out.println("here we go"); 
-                     }
-                     else{
-                         System.out.println("goobye");
-                         break;
-                     }
+                if (n1 == answer2) {
+                    System.out.println("your right!");
+                    i++;
+                    System.out.println("your money is " + i);
+                    System.out.println("if you want to bet again say bet if you would like to withdraw say withdraw");
+                    String answer3 = input.nextLine();
+                    if (answer3.equals("bet")) {
+                        System.out.println("here we go");
                     }
-                 else{
-                     System.out.println("your wrong");
-                      System.out.println("if you want to bet again say bet if you would like to withdraw say withdraw");
-                     String answer4 = input.nextLine();
-                     if(answer4.equals("bet")){
-                         System.out.println("here we go");
-                         }
-                         if(answer4.equals("withdraw")){
-                             System.out.println("goodbye");
-                             break;
-                         }
+                    if (answer1.equals("withdraw")) {
+                        System.out.println("goobye");
+                        break;
+                    }
+                } else {
+                    System.out.println("your wrong");
+                    i++;
+                    System.out.println("if you want to bet again say bet if you would like to withdraw say withdraw");
+                    String answer4 = input.nextLine();
+                    if (answer4.equals("bet")) {
+                        System.out.println("here we go");
+                    }
+                    if (answer4.equals("withdraw")) {
+                        System.out.println("goodbye");
+                        break;
+                    }
 
-                         }
-                         
-                     }
-                     
-           
-                else{
-                System.out.println("if you want to bet on black pick 0 and if you want to bet on red pick 1");
-                 int answer5 = input.nextInt();
-                 System.out.println("how much would you like to bet?");
-                 int answer6 = input.nextInt();
-                 int n2 = rnd.nextInt(2);
-                 System.out.println("genrated number is " + n2);
-
-                 if(n2 == answer5){
-                     System.out.println("correct!");
-                 }
-                 else{
-                     System.out.println("wrong");
-                     System.out.println("if you want to bet again say bet if you would like to withdraw say withdraw");
-                     String answer7 = input.nextLine();
-                     if(answer7.equals("bet")){
-                     }
-                     else{
-                         System.out.println("goobye");
-                         break;
-                     }
-                 }
                 }
+
+            }
+
+            if (answer1.equals("outside")) {
+                System.out.println("if you want to bet on black pick 0 and if you want to bet on red pick 1");
+                int answer5 = input.nextInt();
+                System.out.println("how much would you like to bet?");
+                int answer6 = input.nextInt();
+                int n2 = rnd.nextInt(2);
+                System.out.println("genrated number is " + n2);
+
+                if (n2 == answer5) {
+                    System.out.println("correct!");
+                } else {
+                    System.out.println("wrong");
+                    System.out.println("if you want to bet again say bet if you would like to withdraw say withdraw");
+                    String answer7 = input.nextLine();
+                    if (answer7.equals("bet")) {
+                    } else {
+                        System.out.println("goobye");
+                        break;
+                    }
+                }
+            }
         }
     }
 }
